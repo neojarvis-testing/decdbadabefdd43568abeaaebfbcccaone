5da5691e4241bb442ae754dd471e07b1f5e68586
neojarvis <techadmin@iamneo.ai>
2024-01-09 10:27:46 +0000

    Auto-save-commit-1/9/2024--3:57:40-PM

---
+++ ProductDisplay/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Hello, World!");
+int a=10;
+Console.WriteLine("Hello, World!"+a);
--- dotnetapp/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-Console.WriteLine("##3333333333333333333333");
-Console.WriteLine("*******************");
-    
